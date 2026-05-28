b9c35cd296a19e167258cb181c81a1713a3eabc1
Shubham Raj ( Lucky ) <sr7773879@gmail.com>
2026-05-28 21:19:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 const makeChange = (c) => {
 	let q = Math.floor( c / 25 );
-	amount %= 25;
+	c %= 25;
 	let d = Math.floor( c / 10 );
-	amount %= 10;
+	c %= 10;
 	let n = Math.floor( c / 5 );
-	amount %= 5;
-	let p = amount;
-
+	c %= 5;
+	let p = c;
+	console.log({
+		q: q,
+		d: d,
+		n: n,
+		p: p
+	});
 	return {
 		q: q,
 		d: d,
@@ -14,8 +19,6 @@ const makeChange = (c) => {
 		p: p
 	};
 }
-
-
 
 // Do not the change the code below
 const c = prompt("Enter c: ");
